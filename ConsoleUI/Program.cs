6c7cdff23eaf177483cec55a09760e21a9299d53
Uladzislau_Sakalou <needyoursoul2die@gmail.com>
2015-10-07 22:38:19 +0300

    Added SimpleIterationMethod

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector x = initXVector(1, 1);
+            Matrix A = InitAMatrix();
+            Vector f = A.Multiply(x);
+            double[] tau = {1.0 / (4 * A.GetNorm()), 1.0 / (2 * A.GetNorm()), 1.0 / A.GetNorm()};
+            double[] fault = {0.01, 0.0001, 0.000001};
+            Solver solver = new Solver();
+            Matrix result = new Matrix(3);
+            for(int i = 0; i < 3; ++i)
+                for(int j = 0; j < 3; ++j)
+                    result.Values[i, j] = solver.SimpleIterationMethod(A, f, tau[i], fault[j]);
+            Show(result);
+        }
+
+        private static Matrix InitAMatrix()
+        {
+            double[,] a = new double[3, 3]
+            { { 1.0 / 21, 1.0 / 22, 1.0 / 23 },
+              { 1.0 / 22, 1.0 / 23, 1.0 / 24 },
+              { 1.0 / 23, 1.0 / 24, 1.0 / 25 } };
+            return new Matrix(3, a);
+        }
+
+        private static Vector initXVector(double a, double b)
+        {
+            Random rand = new Random();
+            double[] temp = new double[3];
+            for (int i = 0; i < 3; ++i)
+                temp[i] = a - (b - a) * rand.Next();
+            return new Vector(temp);
+        }
+
+        public static void Show(Matrix matrix)
+        {
+            for (int i = 0; i < 3; ++i)
+            {
+                Console.Write("|");
+                for (int j = 0; j < 3; ++j)
+                {
+                    Console.Write("{0, 7}", matrix.Values[i, j]);
+                    Console.Write("|");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+
+    
+}
---
+++ UnitTesting/TestMatrix.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class TestMatrix
+    {
+        [TestMethod]
+        public void Test_Multiply()
+        {
+            Matrix A = new Matrix(3);
+            double[,] B = new double[3, 3];
+            for (int i = 0; i < 3; ++i)
+                for (int j = 0; j < 3; ++j)
+                    B[i, j] = 2;
+            A.Values = B;
+            double[] d = { 1, 0, 2 };
+            Vector vector = new Vector(d);
+
+            Matrix result = A.Multiply(4);
+            Vector result2 = A.Multiply(vector);
+
+            Assert.AreEqual(8, result.Values[0, 0]);
+            Assert.AreEqual(8, result.Values[0, 2]);
+            Assert.AreEqual(8, result.Values[2, 2]);
+            //Assert.AreEqual(6, result2.Values[0]);
+            //Assert.AreEqual(6, result2.Values[1]);
+            //Assert.AreEqual(6, result2.Values[2]);
+        }
+
+    }
+}
